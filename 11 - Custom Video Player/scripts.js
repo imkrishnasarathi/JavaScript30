8dea629ef95eeac8df4dc5ef2ecb6574975e77b2
Krishna <krishnasarathishem@gmail.com>
2023-06-07 18:46:51 +0530

    change querySelector of skip buttons to qurySelectorAll

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,7 +3,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 function togglePlay() {
