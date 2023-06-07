5948be3a33b643daf62a882948d6d50c3162fd5c
Krishna <krishnasarathishem@gmail.com>
2023-06-07 18:59:22 +0530

    Change querySelector for ranges to querySelectorAll

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
