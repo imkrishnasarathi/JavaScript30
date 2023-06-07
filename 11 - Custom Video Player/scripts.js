ef7dd6599547710b8293115dba8b07d40c79ffb1
Krishna <krishnasarathishem@gmail.com>
2023-06-07 19:18:12 +0530

    Add semicolons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -43,6 +43,8 @@ toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => {
     button.addEventListener('click', skip);
-})
+});
 
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
